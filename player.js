76267b65e9d30aac7fd0ed217b762c29b3b0857a
stu <stu@Ai221LAB0233M30.local>
2017-01-19 21:07:02 -0600

    Initial commit

---
+++ player.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {startPlayer();}, false);
+
+var player;
+
+function startPlayer() {
+    player = document.getElementById('musicPlayer');
+    player.controls = false;
+}
+
+function playAudio() {
+    player.play();
+}
+
+function pauseAudio() {
+    player.pause();
+}
+
+function stopAudio() {
+    player.pause();
+    player.currentTime = 0;
+}
+
+function changeVolume() {
+    player.volume = document.getElementById('changeVolume').value;
+}
